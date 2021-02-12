5e05bed9d88b8677fbc2c6a160a0787f49f78163
TOMMYYEH <z123z14795@gmail.com>
2021-02-12 18:35:24 +0800

    Init a.out final hello.cpp hello1.cpp lalal

---
+++ hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "hello! it's my first time on ubuntu tool for coding!" << endl;
+    cout << "Wanna know that I coding C++ on file and just save with .cpp" << endl;
+    system("pause");
+    return 0;
+}
---
+++ hello1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "hello! it's my first time on ubuntu tool for coding!" << endl;
+    cout << "http link for complie toturol: https://www.youtube.com/watch?v=yXMb7SC9gHg" << endl;
+    return 0;
+}
